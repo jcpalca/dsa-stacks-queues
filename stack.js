ac0e6368f06979b986fbb7b0ef829e4663ccf8a4
byunboy2 <76236947+byunboy2@users.noreply.github.com>
2022-12-12 17:37:12 -0800

    Finished stacks and quenes

--- stack.js
+++ stack.js
@@ -1,13 +1,5 @@
-/** Node: node for a stack. */
+const LinkedList = require("./linked-list");
 
-class Node {
-  val = null;
-  next = null;
-
-  constructor(val) {
-    this.val = val;
-  }
-}
 
 /** Stack: chained-together nodes where you can
  *  remove from the top or add to the top. */
@@ -17,29 +9,37 @@ class Stack {
     this._ll = new LinkedList();
   }
 
+  get top() {
+    return this._ll.head;
+  }
+
+  get size() {
+    return this._ll.length;
+  }
+
   /** push(val): add new value to the top of the stack. Returns undefined. */
 
   push(val) {
-    
+    return this._ll.unshift(val)
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    return this._ll.shift()
   }
 
   /** peek(): return the value of the top node in the stack. */
 
   peek() {
-
+    return this._ll.getAt(0)
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this._ll.length === 0
   }
 }
 
